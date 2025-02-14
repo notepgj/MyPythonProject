697a50936d2b13a222301dd0b4111129d00d2aad
notepgj <pranvera2505@icloud.com>
2025-02-14 23:16:03 +0100

    Add files via upload

---
+++ notepgj.py
@@ -0,0 +1,7 @@
+# Write your code below this line 👇
+print("Hello" + " " + "Pranvera")
+print("Notes from Day 1")
+print("The print statement is used to output strings")
+print("Strings are strings of characters")
+print("Strings Concatenation is done with the + sign")
+print('New lines can be created with a \and the letter n')
